704a1f91d70cadc5984d0771e89b6275a8c5cf0c
John Wass <wassj@ctc.com>
2021-08-16 00:56:36 -0400

    input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::io::Write;
 use std::time::Duration;
+use std::{io, thread};
 
+use termion;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 use tokio::select;
 use tokio::sync::oneshot;
 use tokio::sync::oneshot::Receiver;
@@ -9,17 +14,17 @@ pub struct System;
 
 impl System {
     fn deploy(&self) {
-        println!("deployed, pending cancellation...");
+        println!("deployed!");
     }
 
     pub async fn deploy_or_kill(&self, after: Duration, kill: Receiver<()>) -> Result<(), String> {
         self.deploy();
+        println!("press Enter within {} seconds to keep...", after.as_secs());
 
         if let Err(_) = timeout(after, kill).await {
-            println!("did not receive confirmation within {} secs, killed deployment", after.as_secs());
+            println!("did not receive confirmation, cancelling deployment");
             Ok(())
-        }
-        else {
+        } else {
             println!("confirmed, deployment remains in place!");
             Ok(())
         }
@@ -30,13 +35,25 @@ impl System {
 async fn main() {
     let sys = System;
 
-    let (accept,mut on_accept) = oneshot::channel::<()>();
+    let (accept, mut on_accept) = oneshot::channel::<()>();
     let (kill, mut on_kill) = oneshot::channel::<()>();
 
-    let deployment = sys.deploy_or_kill(Duration::from_secs(2), on_kill);
+    let mut stdin = termion::async_stdin().keys();
+    thread::spawn(move || loop {
+        if let Some(Ok(key)) = stdin.next() {
+            match key {
+                _ => {
+                    accept.send(());
+                    return;
+                }
+            }
+        }
+        thread::sleep(Duration::from_millis(50));
+    });
 
-    select!{
-        _ = deployment => println!("rolled back"),
-        _ = on_accept => println!("accepted"),
+    let deployment = sys.deploy_or_kill(Duration::from_secs(6), on_kill);
+    select! {
+        _ = deployment => println!("rolling back deployment"),
+        _ = on_accept => println!("accepted, deployment remains"),
     }
 }

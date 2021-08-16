f4aead34c6f87bdcd2f905841a926288a87c9f33
John Wass <wassj@ctc.com>
2021-08-16 01:10:42 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,46 @@
-use std::io::Write;
+use std::thread;
 use std::time::Duration;
-use std::{io, thread};
 
 use termion;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use tokio::select;
 use tokio::sync::oneshot;
 use tokio::sync::oneshot::Receiver;
 use tokio::time::timeout;
 
-pub struct System;
+struct System;
 
 impl System {
     fn deploy(&self) {
         println!("deployed!");
     }
 
-    pub async fn deploy_or_kill(&self, after: Duration, kill: Receiver<()>) -> Result<(), String> {
+    async fn deploy_or_kill(&self, after: Duration, on_accept: Receiver<()>) -> Result<(), String> {
         self.deploy();
-        println!("press Enter within {} seconds to keep...", after.as_secs());
+        println!(
+            "press Enter within {} seconds or will rollback...",
+            after.as_secs()
+        );
 
-        if let Err(_) = timeout(after, kill).await {
-            println!("did not receive confirmation, cancelling deployment");
-            Ok(())
+        if let Err(_) = timeout(after, on_accept).await {
+            println!("not accepted, rolling back deployment");
         } else {
-            println!("confirmed, deployment remains in place!");
-            Ok(())
+            println!("accepted, deployment remains")
         }
+        Ok(())
     }
 }
 
 #[tokio::main]
 async fn main() {
     let sys = System;
-
-    let (accept, mut on_accept) = oneshot::channel::<()>();
-    let (kill, mut on_kill) = oneshot::channel::<()>();
+    let (accept, on_accept) = oneshot::channel::<()>();
 
     let mut stdin = termion::async_stdin().keys();
     thread::spawn(move || loop {
         if let Some(Ok(key)) = stdin.next() {
             match key {
                 _ => {
-                    accept.send(());
+                    accept.send(()).expect("failed accept");
                     return;
                 }
             }
@@ -51,9 +48,7 @@ async fn main() {
         thread::sleep(Duration::from_millis(50));
     });
 
-    let deployment = sys.deploy_or_kill(Duration::from_secs(6), on_kill);
-    select! {
-        _ = deployment => println!("rolling back deployment"),
-        _ = on_accept => println!("accepted, deployment remains"),
-    }
+    sys.deploy_or_kill(Duration::from_secs(6), on_accept)
+        .await
+        .unwrap();
 }
